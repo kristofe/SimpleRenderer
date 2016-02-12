6e83f755f78844e139b0507cbe05732e946ca5a3
Kristofer Schlachter <kristofe72@gmail.com>
2016-02-12 12:55:52 -0500

    Starting NORB picture setups

--- source/renderlib/SimpleDFPathTracer.h
+++ source/renderlib/SimpleDFPathTracer.h
@@ -65,6 +65,13 @@ namespace renderlib {
     glm::vec2 _imageDim;
     
     Model _model;
+
+	std::vector<std::string> categories{ "animal","human", "plane", "truck", "car" };
+	std::vector<int> instance{ 0,1,2,3,4,5,6,7,8,9 };//Instances are kind of categories (like lion)
+	std::vector<int> elevations{ 30,35,40,45,50,55,60,65,70 };
+	std::vector<int> azimuths{ 0,20,40,60,80,100,120,140,160,180,200,220,240,260,280,300,320,340 };
+	std::vector<int> lighting{ 0,1,2,3,4,5 };
+
     
     //TextureProxy* _tex;
     Texture _texture;
